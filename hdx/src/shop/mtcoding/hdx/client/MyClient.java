de8576d5e87c198d4055279c716054b9cb620889
metacoding <getinthere@naver.com>
2023-02-05 23:20:08 +0900

    단방향, 반이중 코드 완성

---
+++ hdx/src/shop/mtcoding/hdx/client/MyClient.java
@@ -0,0 +1,38 @@
+package shop.mtcoding.hdx.client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class MyClient {
+
+    private Socket socket;
+    private BufferedWriter request;
+    private BufferedReader response;
+
+    public MyClient() throws Exception {
+        // 1. 서버 연결
+        socket = new Socket("localhost", 10000);
+
+        // 2. 클라이언트 요청함
+        request = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"));
+        request.write("html 페이지 주세요");
+        request.write("\n"); // 버퍼에 끝을 알려주는 프로토콜
+        request.flush(); // 버퍼에 담긴 메시지를 전송
+
+        // 3. 클라이언트 응답 받음
+        response = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+        String msg = response.readLine(); // 버퍼에 \n까지 읽음
+        System.out.println("서버에게서 응답이 왔습니다 : "+msg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            new MyClient();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ hdx/src/shop/mtcoding/hdx/server/MyServer.java
@@ -0,0 +1,40 @@
+package shop.mtcoding.hdx.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServer {
+
+    private ServerSocket serverSocket;
+    private BufferedReader request;
+    private BufferedWriter response;
+
+    public MyServer() throws Exception{
+        // 1. 서버 대기중
+        serverSocket = new ServerSocket(10000);
+        Socket socket = serverSocket.accept();
+        System.out.println("클라이언트가 연결되었습니다");
+
+        // 2. 서버 요청 받음
+        request = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+        String msg = request.readLine(); // 버퍼에 \n까지 읽음
+        System.out.println("클라이언트에게서 요청이 왔습니다 : "+msg);
+
+        // 3. 서버 응답
+        response = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"));
+        response.write("<html><h1>Hello</h1></html>");
+        response.write("\n");
+        response.flush();
+        System.out.println("클라이언트에게 응답합니다");
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            new MyServer();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
